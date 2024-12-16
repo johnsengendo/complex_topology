aeeeab96cc9e3e1b1a67587ed4efb5835597fe07
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-12-16 12:02:48 +0100

    Add files via upload

---
+++ client/install_packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Updating package lists
+apt-get update -y
+
+# Installing useful tools
+apt-get install -y --no-install-recommends \
+    bash \
+    python3 \
+    bash-completion \
+    curl \
+    net-tools
+
+# Installing the packages required for streaming videos and dumping traffic.
+apt-get install -y \
+    ffmpeg \
+    tcpdump \
+    nano 
